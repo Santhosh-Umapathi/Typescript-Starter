fb51a8355d1625b07259241c659322cba8c333eb
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-07-14 22:31:21 +0200

    basics added

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 //Annotations
 let age: number = 20;
+
+//TypeScript Interpreter
+let text = "Hello World";
+let num = 10;
+let isActive = true;
+let any;
+
+let undefinedValue: undefined;
+let nullValue: null;
+
+let strArray: string[] = [];
+let numArray: number[];
+let tuple: [string, number];
